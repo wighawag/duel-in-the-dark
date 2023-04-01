298125e2b5f47b4bc2d59ca44ef171ebc71da643
wighawag <wighawag@gmail.com>
2023-04-01 22:41:28 +0100

    wip: full_game test

--- contracts/test/Game.js
+++ contracts/test/Game.js
@@ -9,10 +9,34 @@ async function solidityProof(fullProof) {
     fullProof.proof,
     fullProof.publicSignals
   );
-  console.log(calldata);
   const actualProof = calldata.slice(0, calldata.indexOf(","));
-  console.log({ actualProof });
   return actualProof;
+}
+
+async function do_move(circuit, contract, values) {
+  const { previous_location, move, new_location, hit, enemy_shot } = values;
+
+  const poseidon = await circomlibjs.buildPoseidon();
+  const new_commit_hash = poseidon.F.toString(poseidon([0, new_location]));
+
+  const fullProof = await circuit.generateProof({
+    publicState: {
+      enemy_shot: enemy_shot,
+    },
+    revealed: {
+      hit: hit ? 1 : 0,
+    },
+    secretState: {
+      previous_location,
+      previous_salt: 0,
+    },
+    secretAction: {
+      move,
+      new_salt: 0,
+      new_location,
+    },
+  });
+  await contract.move(await solidityProof(fullProof), hit, new_commit_hash);
 }
 
 describe("Game", function () {
@@ -35,7 +59,7 @@ describe("Game", function () {
       await games[1].start(first_hash);
 
       await expect(
-        games[0].acceptAndMove("0x00", true, "0x00")
+        games[0].acceptAndMove("0x00", "0x00", true, "0x00")
       ).to.be.revertedWith("INVALID_PROOF");
     });
 
@@ -87,32 +111,9 @@ describe("Game", function () {
         },
       });
 
-      console.log(JSON.stringify(fullProof, null, 2));
-      console.log(
-        JSON.stringify(
-          fullProof.publicSignals.map((v) => BigInt(v).toString(16)),
-          null,
-          2
-        )
-      );
-
-      const solProof = await solidityProof(fullProof);
-      console.log(
-        JSON.stringify(
-          {
-            previous_commit_hash: BigInt(previous_commit_hash).toString(16),
-            solProof,
-            hit,
-            new_commit_hash: BigInt(new_commit_hash).toString(16),
-          },
-          null,
-          2
-        )
-      );
-
       await games[0].acceptAndMove(
         previous_commit_hash,
-        solProof,
+        await solidityProof(fullProof),
         hit,
         new_commit_hash
       );
@@ -121,50 +122,73 @@ describe("Game", function () {
     it("full game", async function () {
       const { games, circuit } = await loadFixture(deployGame);
 
-      const first_hash = "0x00";
+      const first_location = 3;
+      const first_poseidon = await circomlibjs.buildPoseidon();
+      const first_hash = first_poseidon.F.toString(
+        first_poseidon([0, first_location])
+      );
       await games[1].start(first_hash);
 
+      let poseidon;
+      let new_commit_hash;
       let fullProof;
       let hit;
-      let newHash = 0; // TODO
+      let move;
+      let previous_commit_hash;
+      let previous_location;
 
+      previous_location = 1;
+      poseidon = await circomlibjs.buildPoseidon();
+      previous_commit_hash = poseidon.F.toString(
+        poseidon([0, previous_location])
+      );
+      move = 1;
+      new_location = 2;
+      poseidon = await circomlibjs.buildPoseidon();
+      new_commit_hash = poseidon.F.toString(poseidon([0, new_location]));
+      hit = false;
       fullProof = await circuit.generateProof({
         publicState: {
-          enemy_shot: 3,
+          enemy_shot: 0,
         },
         revealed: {
           hit: hit ? 1 : 0,
         },
         secretState: {
-          previous_location: 1,
+          previous_location,
           previous_salt: 0,
+          previous_commit_hash,
         },
         secretAction: {
-          move: 1,
+          move,
           new_salt: 0,
-          new_location: 2,
+          new_location,
+          new_commit_hash,
         },
       });
-      await games[0].move(proofToSolidityInput(fullProof.proof), hit, newHash);
 
-      fullProof = await circuit.generateProof({
-        publicState: {
-          enemy_shot: 3,
-        },
-        revealed: {
-          hit: hit ? 1 : 0,
-        },
-        secretState: {
-          previous_location: 1,
-          previous_salt: 0,
-        },
-        secretAction: {
-          move: 1,
-          new_salt: 0,
-          new_location: 2,
-        },
+      await games[0].acceptAndMove(
+        previous_commit_hash,
+        await solidityProof(fullProof),
+        hit,
+        new_commit_hash
+      );
+
+      await do_move(circuit, games[1], {
+        previous_location: first_location,
+        move: 3,
+        new_location: 2,
+        hit: false,
+        enemy_shot: 0,
       });
-      await games[1].move(proofToSolidityInput(fullProof.proof), hit, newHash);
+
+      // await do_move(circuit, games[0], {
+      //   previous_location: 3,
+      //   move: 3,
+      //   new_location: 2,
+      //   hit: false,
+      //   enemy_shot: 0,
+      // });
     });
   });
 });
